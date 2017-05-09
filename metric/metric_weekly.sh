4ab6fd09eb16259c619fea8da617769ab9f6cd07
Justin Leong <justin.leong@ubc.ca>
2017-05-09 10:00:35 -0700

    Clean up outdated files

--- metric/metric_weekly.sh
+++
@@ -1,3 +0,0 @@
-cd /Users/katrinani/Documents/CTLT/analysis/metric
-jupyter nbconvert --to html --execute multi_courses.ipynb --ExecutePreprocessor.timeout=500
-mv multi_course.html '/Users/katrinani/Google Drive/Data scripts/metrics'
